a2727ba219ae5e4ad64ae2e15e2bd8b8082b8b24
martin-vasilev <martin.r.vasilev@gmail.com>
2018-08-24 14:46:43 +0100

    cleaned up ocr

--- R/ocr.r
+++ R/ocr.r
@@ -3,14 +3,12 @@
 
 # tentative code for reading in image stimuli:
 
-#library(magick)
-#library(tesseract)
+OCR<- function(image_file= "C:/Users/Martin Vasilev/Documents/Oz/Oz/Dorothy/Dorothy2.bmp",
+               language= "English"){
 
-### functions:
+  # Additional functions:
+  numerize<- function(string){as.numeric(unlist(strsplit(string, ",")))}
 
-### OCR<- function(image_file, language= "English"){
-
-   language= "English"
   # default language is English
   # check if user defined another available language (gives better accuracy)
    allowedLangs<- c("Spanish", "Dutch", "Korean", "French", "Japanese")
@@ -31,38 +29,34 @@
       useLang<- tesseract::tesseract("eng")
    }
 
-#}
+   ### Start OCR:
+   # read in image:
+   input <- magick::image_read(image_file)
 
-#--------------------------------------------------------------------------#
-numerize<- function(string){as.numeric(unlist(strsplit(string, ",")))}
-#--------------------------------------------------------------------------#
+   # Recognize text image:
+   text <- tesseract::ocr(input, engine= useLang)
 
-image_file<-"C:/Users/Martin Vasilev/Documents/Oz/Oz/Dorothy/Dorothy2.bmp"
+   # Get the pixel locations of words:
+   loc<- tesseract::ocr_data(input , engine= useLang)
 
-# read in image:
-input <- magick::image_read(image_file)
+   # make coords in a matrix:
+   coords<- as.data.frame(t(matrix(data = numerize(loc$bbox), nrow = 4)))
+   colnames(coords)<- c("x1", "y1", "x2", "y2")
+   coords$conf<- loc$confidence
+   coords$word<- loc$word
 
-# Recognize text image:
-text <- tesseract::ocr(input) #, engine= useLang
+   # I need to parse the text so that I know which words are line final;
+   # I need to adjust y1 and y2 to be the same for all words on the line-
+   # to do that, take min(y1) and max (y2). This will give us the max text
+   # window on the line.
 
-# Get the pixel locations of words:
-loc<- tesseract::ocr_data(input) #, engine= useLang
+   # Think also about what to do with empty spaces:
 
-# make coords in a matrix:
-coords<- as.data.frame(t(matrix(data = numerize(loc$bbox), nrow = 4)))
-colnames(coords)<- c("x1", "y1", "x2", "y2")
-coords$conf<- loc$confidence
-coords$word<- loc$word
-
-# I need to parse the text so that I know which words are line final;
-# I need to adjust y1 and y2 to be the same for all words on the line-
-# to do that, take min(y1) and max (y2). This will give us the max text
-# window on the line.
-
-# Think also about what to do with empty spaces:
-
-lines<- unlist(strsplit(text, "\n"))
-if(lines[length(lines)]==""){
-  lines<- lines[1:(length(lines)-1)]
+   return(coords)
 }
 
+
+#lines<- unlist(strsplit(text, "\n"))
+#if(lines[length(lines)]==""){
+#  lines<- lines[1:(length(lines)-1)]
+#}
