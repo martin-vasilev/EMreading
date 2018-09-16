8e555ddf6493961cde7410df0dfd17055da7efd1
martin-vasilev <martin.r.vasilev@gmail.com>
2018-09-16 20:45:21 +0100

    updates to cleanData.R

--- R/cleanData.R
+++ R/cleanData.R
@@ -3,7 +3,7 @@
 #' 
 #' @author Martin R. Vasilev
 #' 
-#' @param data Dataframe containing the raw fixation data that were extracted with the
+#' @param raw_fix Dataframe containing the raw fixation data that were extracted with the
 #' EMreading package
 #' 
 #' @param removeOutsideText A logical inicating whether to remove all fixations outside the text 
@@ -12,18 +12,37 @@
 #' @param removeBlinks A logical indicating whether to exclude all blinks from the data. The 
 #' default (and recommended) setting is TRUE.
 #' 
+#' @param combineNearbySmallFix A logical indicating whether to merge small fixations < 80 ms 
+#' that are within 1 character space of another fixation (the small fixation will be merged with
+#'  the nearby fixation). The default is TRUE.
+#'  
+#' @param removeSmallFix A logical indicating whether to remove any remaining fixations smaller
+#' than a certain cut-off. The default is TRUE
+#' 
+#' @param smallFixCutoff A numerical value indiciating the cut-off for removing small 
+#' fixations (the default is 80 ms). Any values smaller than this number will be removed.
+#' 
 #' @param removeOutliers A logical indicating whether to remove outliers from the data (default
 #' is TRUE). This parameter also requires the specification of a method and cutoff for
 #' outlier exclusion (see below)
 #' 
-#' @param outlierCutoff Specifies the exclusion of long fixations (outliers). If set to FALSE,
-#' no outliers will be exluded. If set to a number, fixations greater than that number will be
-#' excluded as outliers. Default is to exclude fixations longer than 800 ms as outliers.
+#' @param outlierMethod A string indicating what type of outlier exclusion method to use.
+#' Allowed are 2 methods: "ms" for exclusion of fixations based on a number in milliseconds;
+#' or "std" for exlusion based on values that are a certain number of standard deviations above
+#' the subject's mean.
 #' 
+#' @param outlierCutoff Numerical value that specifies the cut-off for removing outliers. If you 
+#' chose "ms" in "outlierMethod", please specify a number in ms (e.g., 800). if you chose "std",
+#' please enter a number in standard deviations above the mean (e.g., 3).
+#' 
+#' @param silent A logical indicating whether to print output regarding merged fixations. 
+#' Set to FALSE if you don't want this output (default is TRUE).
  
-cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE, 
+cleanData<- function(raw_fix= data, removeOutsideText= TRUE, removeBlinks= TRUE, 
+                     combineNearbySmallFix= TRUE, removeSmallFix= TRUE, smallFixCutoff= 80,
                      removeOutliers= TRUE, outlierMethod= "ms", outlierCutoff= 800,
-                     removeSmallFix= TRUE, smallFixCutoff= 80, combineNearbySmallFix= TRUE){
+                     silent= FALSE){
+  
   # check user input:
   if(!is.logical(removeOutliers)){
     stop("removeOutliers can only be set to TRUE or FALSE")
@@ -40,7 +59,12 @@ cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE,
   }
   
   if(toupper(outlierMethod)== "STD" & outlierCutoff> 3 | toupper(outlierMethod)== "STD" & outlierCutoff< 2.5){
-    warning("This is unusual cutoff. Typical values are between 2.5-3 STD")
+    warning("This is unusual cut-off. Typical values are between 2.5-3 STD")
+  }
+  
+  
+  if(removeSmallFix & smallFixCutoff<50 | removeSmallFix & smallFixCutoff>100){
+    warning("Unusual cut-off for removing short fixations: typical values are between 50-100 ms")
   }
   
   ### Print settings back to user:
@@ -74,9 +98,8 @@ cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE,
     s5<- ''
   }
   
-  cat(paste("I will", s1, s2, s3, s4, s5, sep=', '))
-  cat("\n")
-  
+  cat(paste("I will", s1, s2, s3, s4, s5, sep='\n  - '))
+  cat("\n\n\n")
   
     if(combineNearbySmallFix){
       which_comb<- NULL
@@ -91,28 +114,36 @@ cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE,
            after<- abs(raw_fix$char_trial[i]- raw_fix$char_trial[i+1])
            if(prev== 1){
              which_comb<- c(which_comb, i)
-             cat(paste("\nsub ", raw_fix$sub[i], ", trial ", raw_fix$item[i],
-                   ":  fix ", raw_fix$fix_num[i], " (", raw_fix$fix_dur[i],
-                   " ms)", " was merged with fix ", raw_fix$fix_num[i-1],
-                   " (", raw_fix$fix_dur[i-1], " ms)",
-                   " -> new fix ", raw_fix$fix_num[i-1],
-                   " (",  raw_fix$fix_dur[i]+ raw_fix$fix_dur[i-1], 
-                   "ms)", sep=''))
-             cat("\n")
+             
+             if(!silent){
+               cat(paste("\nsub ", raw_fix$sub[i], ", trial ", raw_fix$item[i],
+                         ":  fix ", raw_fix$fix_num[i], " (", raw_fix$fix_dur[i],
+                         " ms)", " was merged with fix ", raw_fix$fix_num[i-1],
+                         " (", raw_fix$fix_dur[i-1], " ms)",
+                         " -> new fix ", raw_fix$fix_num[i-1],
+                         " (",  raw_fix$fix_dur[i]+ raw_fix$fix_dur[i-1], 
+                         "ms)", sep=''))
+               cat("\n")
+             }
+
              # merge only AFTER printing output!
              raw_fix$fix_dur[i-1]<- raw_fix$fix_dur[i-1] + raw_fix$fix_dur[i]
             
            }
            if(after== 1){
              which_comb<- c(which_comb, i)
-             cat(paste("\nsub ", raw_fix$sub[i], ", trial ", raw_fix$item[i],
-                       ":  fix ", raw_fix$fix_num[i], " (", raw_fix$fix_dur[i],
-                       " ms)", " was merged with fix ", raw_fix$fix_num[i+1],
-                       " (", raw_fix$fix_dur[i+1], " ms)",
-                       " -> new fix ", raw_fix$fix_num[i+1],
-                       " (",  raw_fix$fix_dur[i]+ raw_fix$fix_dur[i+1], 
-                       "ms)", sep='')) 
-             cat("\n")
+             
+             if(!silent){
+               cat(paste("\nsub ", raw_fix$sub[i], ", trial ", raw_fix$item[i],
+                         ":  fix ", raw_fix$fix_num[i], " (", raw_fix$fix_dur[i],
+                         " ms)", " was merged with fix ", raw_fix$fix_num[i+1],
+                         " (", raw_fix$fix_dur[i+1], " ms)",
+                         " -> new fix ", raw_fix$fix_num[i+1],
+                         " (",  raw_fix$fix_dur[i]+ raw_fix$fix_dur[i+1], 
+                         "ms)", sep='')) 
+               cat("\n")
+             }
+
              raw_fix$fix_dur[i+1]<- raw_fix$fix_dur[i+1] + raw_fix$fix_dur[i]
            }
            
@@ -154,10 +185,17 @@ cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE,
   }
   nOutlier<- nstart- nOutBnds - nblink- nrow(raw_fix)
   
+  if(removeSmallFix){
+    raw_fix<- subset(raw_fix, fix_dur>= smallFixCutoff)
+  }
+  nSmallFix<- nstart- nOutBnds - nblink- nOutlier- nrow(raw_fix)
+  
+  
   output<- paste("\nRemoved fixations: \n", "- outside of text or screen area: ", 
                  round((nOutBnds/nstart)*100, 4), " % \n",
                  "- due to blinks: ", round((nblink/nstart)*100, 4), " % \n",
-                 "- outliers: ", round((nOutlier/nstart)*100, 4), " % \n \n",
+                 "- outliers: ", round((nOutlier/nstart)*100, 4), " % \n",
+                 "- Small fixations: ", round((nSmallFix/nstart)*100, 4), " % \n \n",
                  "Remaining fixations: ", round((nrow(raw_fix)/nstart)*100, 4),
                  " % \n", sep = '')
   
@@ -165,7 +203,7 @@ cleanData<- function(data= raw_fix, removeOutsideText= TRUE, removeBlinks= TRUE,
   
   
   if(removeOutliers & nrow(o)>0 & length(unique(o$cond))>0){
-    test<- chisq.test(table(o$cond))
+    test<- suppressWarnings(chisq.test(table(o$cond)))
     
     if(test$p.value<0.05){
       warning("\n Chi-square test detects enequal number of outliers excluded per condition! \n")
