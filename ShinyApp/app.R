659ad3d6731d63455e338bc9c8434ef763285bbc
martin-vasilev <martin.r.vasilev@gmail.com>
2018-10-07 05:13:08 +0100

    pre-processing works but still can't download result

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyalert)
 
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
@@ -13,15 +14,15 @@ ui <- fluidPage(
     sidebarPanel(
       
       # X screen Resolution:
-      numericInput("ResX", "X screen resolution (in pixels):", 1024, min = 1, max = 10000),
+      numericInput("ResX", "X screen resolution (in pixels):", 1920, min = 1, max = 10000),
       verbatimTextOutput("value1"),
 
       # Y screen Resolution:
-      numericInput("ResY", "Y screen resolution (in pixels):", 768, min = 1, max = 10000),
+      numericInput("ResY", "Y screen resolution (in pixels):", 1080, min = 1, max = 10000),
       verbatimTextOutput("value2"),
       # 
       # Y screen Resolution:
-      numericInput("maxtrial", "Maximum number of trials:", 0, min = 1, max = 10000),
+      numericInput("maxtrial", "Maximum number of trials:", 120, min = 1, max = 10000),
       verbatimTextOutput("value3"),
       
       # Y screen Resolution:
@@ -38,9 +39,27 @@ ui <- fluidPage(
     
     # Main panel for displaying outputs ----
     mainPanel(
-      
+      useShinyalert(),
+      tags$head(tags$style(type="text/css", "
+             #loadmessage {
+                           position: fixed;
+                           top: 0px;
+                           left: 0px;
+                           width: 100%;
+                           padding: 5px 0px 5px 0px;
+                           text-align: center;
+                           font-weight: bold;
+                           font-size: 100%;
+                           color: #000000;
+                           background-color: #CCFF66;
+                           z-index: 105;
+                           }
+                           ")),
+      conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                       tags$div("Processing data...",id="loadmessage")),
       # Output: Verbatim text for data summary ----
-      verbatimTextOutput("summary")
+      verbatimTextOutput("summary"),
+      downloadButton("downloadData", "Download Data")
       
       # Output: HTML table with requested number of observations ----
       #textOutput("summary")
@@ -55,18 +74,91 @@ server <- function(input, output) {
   # set max seze to 100 MB to allow uploading larger files:
   options(shiny.maxRequestSize=100*1024^2)
   
+ 
   output$summary <- renderPrint({
     dataFile= NULL
     file1 = input$file1
     if (is.null(file1)) {
       return(cat("Please upload data file"))
+    }else{
+
+
+      dataFile= readLines(file1$datapath)
+      
+      source("https://raw.githubusercontent.com/martin-vasilev/EMreading/master/R/utility.R")
+      #head(dataFile)
+      trial_db<- trial_info(dataFile, input$maxtrial)
+      #head(trial_db)
+      for(j in 1:nrow(trial_db)){ # for each item
+        if(j!= nrow(trial_db)){
+          time= 1000*15
+        }else{
+          time= 500
+        }
+        shinyalert(
+          title = paste("Trial", j),
+          text = "",
+          closeOnEsc = TRUE,
+          closeOnClickOutside = FALSE,
+          html = FALSE,
+          type = "info",
+          showConfirmButton = FALSE,
+          showCancelButton = FALSE,
+          timer = time,
+          imageUrl = "",
+          animation = TRUE
+        )
+
+        text<- get_text(dataFile[trial_db$ID[j]:trial_db$start[j]]) # get text details (Eyetrack)
+        
+        if(text[1]!=0){ # if trial contained text
+          try(coords<- get_coord(text)) # extract text coordinates
+          map<- coord_map(coords, x=input$ResX, y= input$ResY) # map them to pixels on the screen
+          
+          # Extract raw fixations from data and map them to the text:
+          try(raw_fix_temp<- parse_fix(dataFile, map, coords, trial_db[j,], 1,
+                                       input$ResX, input$ResY, input$tBlink, SL= TRUE))
+          
+          # Combine fixations:
+          if(is.null(raw_fix_temp)){
+            next;
+          }
+          raw_fix<- rbind(raw_fix, raw_fix_temp)
+          
+          if(length(raw_fix_temp)>1 & plot==TRUE){ # if data was extracted from trial
+            # create picture of fixations:
+            plot_fix(coords, raw_fix_temp, i, j, ResX, ResY)
+          }
+        } else{ # if there was no text in trial, just extract fixations
+          try(raw_fix_temp<- parse_fix(dataFile, map=0, coords=0, trial_db[j,], 1, input$ResX,
+                                       input$ResY, input$tBlink, hasText=FALSE, SL= TRUE))
+          if(is.null(raw_fix_temp)){
+            next;
+          }
+          #raw_fix_temp$sub<- i
+          raw_fix<- rbind(raw_fix, raw_fix_temp)
+          # create picture of fixations:
+          
+          #if(length(raw_fix_temp)>1 & plot==TRUE){ # if data was extracted from trial
+          #  plot_fix(coords, raw_fix_temp, i, j, ResX, ResY, hasText = FALSE)
+          #}
+        }
+        
+        #cat(toString(j)); cat(" ")
+      } # end of item loop
+      
     }
-    
-    dataFile= readLines(file1$datapath)
-      #dataset <- datasetInput()
-      #summary(dataset)
-    head(dataFile)
+
   })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(raw_fix, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(raw_fix, file)
+    }
+  )
   
 }
 
